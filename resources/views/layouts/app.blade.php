936309ac670cd1dd0e137182c368a559239e7fca
DemianWHT <demiankost2@gmail.com>
2023-12-02 00:27:33 +0100

    First commit of app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel Domain-Driven-Design Example</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+        @vite('resources/css/app.css')
+        @livewireStyles
+
+        @vite('resources/js/app.js')
+        @livewireScripts
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>

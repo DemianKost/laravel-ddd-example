ab19ed53f0e0a4a4771762caeffc8411a7c10762
DemianWHT <demiankost2@gmail.com>
2023-12-02 02:00:40 +0100

    Moved dashboard to a controller

---
+++ app/Http/Controllers/Web/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class DashboardController
+{
+    /**
+     * @param Request $request
+     * @return View
+     */
+    public function __invoke(Request $request): View
+    {
+        return view('dashboard', [
+            'user' => auth()->user()
+        ]);
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-4 p-6 flex items-center justify-between">
+                <h2 class="text-4xl text-gray-900 font-bold">Good morning, {{ auth()->user()->name }}!</h2>
+                <button class="px-4 py-2 bg-red-500 text-white rounded-md">Complete your profile</button>
             </div>
         </div>
     </div>

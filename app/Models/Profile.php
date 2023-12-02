2ab74d43df6ab17a741007a0a01b42b866318ea9
DemianWHT <demiankost2@gmail.com>
2023-12-02 01:08:37 +0100

    Added UUID trait and included this inside Profile model

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\Concerns\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Profile extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuid;
 
     protected $fillable = [
         'uuid',
